4de585ddc2cc7276515e2940cf3448f48e67bafb
quanghuy010120 <phamquanghuy010100@gmail.com>
2022-01-19 13:51:36 +0700

    Create_ProJect_baiThucHanh19-1

--- baiThucHanhSo2/src/App.java
+++ baiThucHanhSo2/src/App.java
@@ -1,5 +1,8 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("baiThucHanhSo2!");
-    }
-}
+
+/*
+ * public class App {
+ * public static void main(String[] args) throws Exception {
+ * System.out.println("baiThucHanhSo2!");
+ * }
+ * }
+ */
