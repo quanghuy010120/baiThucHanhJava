7f02c3267d08dc702fb3e8b93b74d1b5eec1921d
quanghuy010120 <phamquanghuy010100@gmail.com>
2022-02-16 15:03:21 +0700

    baiThuchanh1602

--- baiThuchanh1602/src/App35.java
+++ baiThuchanh1602/src/App35.java
@@ -1,36 +1,17 @@
-
 import java.util.Scanner;
 
 public class App35 {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        int firstNumber, secondNumber;
+        System.out.println("Nhập số thứ nhất: ");
         Scanner sc = new Scanner(System.in);
+        firstNumber = sc.nextInt();
+        System.out.println("Nhập số thứ hai: ");
+        secondNumber = sc.nextInt();
+        int minNumber = firstNumber;
+        if (firstNumber > secondNumber)
+            minNumber = secondNumber;
+        System.out.println("Số nhỏ nhất trong 2 số " + firstNumber + " và " + secondNumber + " là : " + minNumber);
 
-        System.out.print("Nhap vao so luong phan tu: n = ");
-        int n = sc.nextInt();
-
-        int[] a = new int[n];
-
-        System.out.println("Nhap vao mang cac so nguyen:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("\tPhan tu thu " + (i + 1) + " la: ");
-            a[i] = sc.nextInt();
-        }
-
-        System.out.println("Mang vua nhap la:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + "\t");
-        }
-
-        int min = a[0];
-        for (int i = 1; i < n; i++) {
-            if (min > a[i]) {
-                min = a[i];
-            }
-        }
-
-        System.out.println("\nPhan tu nho nhat cua mang la:" + min);
-
-        sc.close();
     }
 }
